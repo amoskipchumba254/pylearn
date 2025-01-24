6d4345670ff67f79b6d177f2acee4045bac6d3fa
amoskipchumba254 <125817518+amoskipchumba254@users.noreply.github.com>
2025-01-24 16:39:19 +0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -624,3 +624,261 @@ for user in new_users:
         print(f"Sorry {user}, that username is taken.")
     else:
         print(f"Great, {user} is available.")
+        
+# dictionaries
+# a simple dictionary
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+alien_0 = {'color': 'green', 'points': 5}
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+# starting with an empty dictionary
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# modifying values in a dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+alien_0['color'] = 'yellow'
+
+print(f"The alien is now {alien_0['color']}.")
+
+# tracking the position of an alien that can move at different speeds
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+# move the alien to the right
+# determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be a fast alien
+    x_increment = 3
+
+# the new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New position: {alien_0['x_position']}")
+
+# removing key-value pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+language = favorite_language['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
+
+# using get() to access values
+alien_0 = {'color': 'green', 'speed': 'slow'}
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
+
+# looping through all key-value pairs
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi'
+}
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+    
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+for name, language in favorite_language.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+    
+# looping through all the keys in a dictionary
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+for name in favorite_language.keys():
+    print(name.title())
+    
+favorite_language = {
+    
+}
+
+friends = ['phil', 'sarah']
+for name in favorite_language.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_language[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+        
+if 'erin' not in favorite_language.keys():
+    print("Erin, please take our poll!")
+    
+# looping through a dictionary's keys in order
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+for name in sorted(favorite_language.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+    
+# looping through all values in a dictionary
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+    
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+# nesting dictionaries
+# a list of dictionaries
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+    
+# make an empty list for storing aliens
+aliens = []
+
+# make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# show how many aliens have been created
+print(f"Total number of aliens: {len(aliens)}")
+
+# make an empty list for storing aliens
+aliens = []
+
+# make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+        
+# show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+    
+# store information about a pizza being ordered 
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese']
+}
+
+# summarize the order
+print(f"You ordered a {pizza['crust']}-crust pizza " 
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print("\t" + topping)
+    
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell']
+}
+
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
+
+# a dictionary in a dictionary
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton'
+    },
+    
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris'
+    }
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+    
+    print(f"\tFull_name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
+# modulo operator
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\n?The number {number} is odd.")
